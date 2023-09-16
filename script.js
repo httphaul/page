23a2b1084118812c119559cc1fd99359488a3290
httphaul <pcrain@gmail.com>
2023-09-16 18:03:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const killfeed = document.getElementById("killfeed");
+const hoverRectangle = document.getElementById("hoverRectangle");
+
+// Show the killfeed and rectangle on hover
+hoverRectangle.addEventListener("mouseenter", () => {
+    killfeed.style.display = "block";
+});
+
+// Hide the killfeed and rectangle on mouseout
+hoverRectangle.addEventListener("mouseout", () => {
+    killfeed.style.display = "none";
+});
+
+// Fetch data from killfeed.txt (you can replace this with your own data source)
+fetch("killfeed.txt")
+    .then(response => response.text())
+    .then(data => {
+        // Split the data into lines
+        const lines = data.split("\n");
+        
+        // Display the killfeed data
+        killfeed.innerHTML = lines.map(line => `<p>${line}</p>`).join("");
+    })
+    .catch(error => console.error(error));
